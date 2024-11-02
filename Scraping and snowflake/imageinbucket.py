afc33656daacb9628ce7663df5ba73c327f7e39f
Shubham Agarwal <agarwal.shubh@northeastern.edu>
2024-11-02 14:31:31 -0400

    Update imageinbucket.py
    Removed AWS bucket ID and keys.

--- Scraping and snowflake/imageinbucket.py
+++ Scraping and snowflake/imageinbucket.py
@@ -12,8 +12,8 @@ import boto3
 
 # AWS S3 setup with your provided credentials
 s3 = boto3.client('s3',
-                  aws_access_key_id='AKIAQIJRSDTXXVW6DVT5',
-                  aws_secret_access_key='s0Q/T4oYaiEmC3lCOKxrAVuwr6wUEZ6y22mzT+9/',
+                  aws_access_key_id='',
+                  aws_secret_access_key='',
                   region_name='us-east-1')
 
 # S3 bucket name
